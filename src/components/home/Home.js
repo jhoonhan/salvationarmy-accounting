533134353bd984025c205c05f5d8da9119877543
jhoonhan <jhoon.han@outlook.com>
2022-04-06 11:00:55 -0400

    3/6

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,19 +4,248 @@ const Home = () => {
   return (
     <>
       <div className="home__container">
-        <div className="nav__side">
+        <div className="home__col home__col--1">
           <div className="nav__side__profile">profile</div>
-          <nav>
+          <nav className="nav__side">
             <ul>
-              <li>aaang</li>
-              <li>aaang</li>
-              <li>aaang</li>
-              <li>aaang</li>
+              <li>Home</li>
+              <li>Reports</li>
+              <li>Users</li>
+              <li>Orders</li>
             </ul>
           </nav>
         </div>
-        <div className="home__section">aang</div>
-        <div className="home__section">aang</div>
+        <div className="home__col home__col--2">
+          <div className="flex__vertical">
+            <div className="ui__container">
+              <label>Montly Report</label>
+              <div className="flex__vertical">
+                <div>
+                  <div className="chart">
+                    <div className="chart__vertical-label">
+                      <div>$1000</div>
+                      <div>$900</div>
+                      <div>$800</div>
+                      <div>$700</div>
+                      <div>$600</div>
+                      <div>$500</div>
+                      <div>$400</div>
+                      <div>$300</div>
+                      <div>$200</div>
+                      <div>$100</div>
+                      <div>$0</div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "90%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "20%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "30%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "40%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "50%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "60%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "70%" }}
+                      ></div>
+                    </div>
+                    <div className="chart__graph">
+                      <div
+                        className="chart__bar"
+                        style={{ height: "80%" }}
+                      ></div>
+                    </div>
+                  </div>
+                  <div className="chart__horizontal-label">
+                    <div></div>
+                    <div>4/3</div>
+                    <div>4/10</div>
+                    <div>4/17</div>
+                    <div>4/24</div>
+                    <div>5/3</div>
+                    <div>5/10</div>
+                    <div>5/17</div>
+                    <div>5/24</div>
+                  </div>
+                </div>
+                <div className="table">
+                  <div></div>
+                  <div>4/3</div>
+                  <div>4/10</div>
+                  <div>4/17</div>
+                  <div>4/24</div>
+                  <div>5/3</div>
+                  <div>5/10</div>
+                  <div>5/17</div>
+                  <div>5/24</div>
+
+                  <div>Cart.</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+
+                  <div>Offer.</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+
+                  <div>Thnks.</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+
+                  <div>S & W</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+
+                  <div>Build.</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+
+                  <div>Total</div>
+                  <div>$ 1000.00</div>
+                  <div>$ 980.00</div>
+                  <div>$ 1140.00</div>
+                  <div>$ 1200.00</div>
+                  <div>$ 960.00</div>
+                  <div>$ 523.00</div>
+                  <div>$ 990.00</div>
+                  <div>$ 125.00</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="ui__container">
+              <label>Corps Stats</label>
+              <div className="home__stats">
+                <p>Members :</p>
+                <p>36 members</p>
+
+                <p>Weekly Attendance :</p>
+                <p>16 members</p>
+
+                <p>Monthly Avg :</p>
+                <p>$ 980.00</p>
+
+                <p>Weekly Avg :</p>
+                <p>$ 980.00</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex__vertical">
+            <div className="ui__container">
+              <label>Attendance Report</label>
+              <div>
+                <div className="chart">
+                  <div className="chart__vertical-label">
+                    <div>$1000</div>
+                    <div>$900</div>
+                    <div>$800</div>
+                    <div>$700</div>
+                    <div>$600</div>
+                    <div>$500</div>
+                    <div>$400</div>
+                    <div>$300</div>
+                    <div>$200</div>
+                    <div>$100</div>
+                    <div>$0</div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "90%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "20%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "30%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "40%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "50%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "60%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "70%" }}></div>
+                  </div>
+                  <div className="chart__graph">
+                    <div className="chart__bar" style={{ height: "80%" }}></div>
+                  </div>
+                </div>
+                <div className="chart__horizontal-label">
+                  <div></div>
+                  <div>4/3</div>
+                  <div>4/10</div>
+                  <div>4/17</div>
+                  <div>4/24</div>
+                  <div>5/3</div>
+                  <div>5/10</div>
+                  <div>5/17</div>
+                  <div>5/24</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
