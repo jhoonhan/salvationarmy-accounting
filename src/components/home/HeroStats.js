a6911c13fbcbe5a4f6ab9acd0d4e4c1c82f8de75
jhoonhan <jhoon.han@outlook.com>
2022-04-06 19:41:14 -0400

    all this in a custom hook

--- src/components/home/HeroStats.js
+++ src/components/home/HeroStats.js
@@ -1,40 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { render } from "react-dom";
 import useDateSetter from "../hooks/useDateSetter";
 
 const HeroStats = ({ reports }) => {
-  console.log(reports);
   const [dates, setDates] = useDateSetter();
-
-  const reportsThisLastWeek = reports.filter(
-    (report) =>
-      report.date === dates.currentDate || report.date === dates.prevtDate
-  );
-
-  const reportsThisMonth = reports.map((report) => {
-    const fuck = dates.sundaysThisMonth.filter((el) => {
-      return el === report.date;
-    });
-    console.log(fuck);
-    return fuck;
+  const [filteredReports, setFilteredReports] = useState({
+    monthTotal: 0,
+    reportsThisMonth: [],
+    thisLastDiff: 0,
+    reportsThisLastWeek: [],
+    attendance: 0,
   });
-  console.log(reportsThisMonth);
+
+  useEffect(() => {
+    const reportsThisLastWeek = reports.filter(
+      (report) =>
+        report.date === dates.currentDate || report.date === dates.prevDate
+    );
+
+    let reportsThisMonth = [];
+    reports.forEach((report) => {
+      const filtered = dates.sundaysThisMonth.filter(
+        (date) => date === report.date
+      );
+      if (filtered[0]) reportsThisMonth.push(report);
+    });
+
+    const thisLastDiff = reportsThisLastWeek
+      .map((report) => report.total)
+      .reduce((a, b) => a - b, 0);
+
+    const monthTotal = reportsThisMonth
+      .map((report) => report.total)
+      .reduce((a, b) => a + b, 0);
+
+    const attendance = reports.filter(
+      (report) => report.date === dates.prevDate
+    )[0].orders.length;
+
+    setFilteredReports({
+      ...filteredReports,
+      thisLastDiff,
+      reportsThisLastWeek,
+      reportsThisMonth,
+      monthTotal,
+      attendance,
+    });
+  }, [reports]);
+
+  useEffect(() => {
+    console.log(filteredReports);
+  }, [filteredReports]);
 
   const render = () => {
+    const {
+      monthTotal,
+      reportsThisMonth,
+      thisLastDiff,
+      reportsThisLastWeek,
+      attendance,
+    } = filteredReports;
     return (
       <article className="ui__container">
         <div className="home__prev">
           <div>
             <h3>From last week</h3>
-            <p className="text--large">-$100.00</p>
+            <p className="text--large">${thisLastDiff.toFixed(2)}</p>
           </div>
           <div>
             <h3>Total This Month</h3>
-            <p className="text--large">$920.00</p>
+            <p className="text--large">${monthTotal.toFixed(2)}</p>
           </div>
           <div>
-            <h3>Attendance</h3>
-            <p className="text--large">16</p>
+            <h3>Last Attendance</h3>
+            <p className="text--large">{attendance}</p>
           </div>
         </div>
       </article>
