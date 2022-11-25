9442d446108e9910543615229ba582d17f87d219
Joe Han <jhoon.han@outlook.com>
2022-11-24 21:10:45 -0500

    implementing v.3 with user report

--- src/components/order/OrderChart.js
+++ src/components/order/OrderChart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { capitalizeName, lastFirstExt, getName } from "../helpers/nameHelper";
 import OrderDeleteButton from "./OrderDeleteButton";
 
