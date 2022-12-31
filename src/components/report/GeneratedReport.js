d7ee4d3d0a0c4af583e6c3a8b9e92c1eef21e0e2
Joe Han <jhoon.han@outlook.com>
2022-12-31 00:42:42 -0500

    print works

--- src/components/report/GeneratedReport.js
+++ src/components/report/GeneratedReport.js
@@ -70,6 +70,81 @@ const GeneratedReport = ({ orders, users, selectedUser, selectedYear }) => {
     });
   };
 
+  const renderTable = (orders, totals) => {
+    return (
+      <div
+        className="flex__vertical"
+        style={{
+          minHeight: "500px",
+          backgroundColor: "white",
+        }}
+      >
+        <div>
+          <label>Generated Report</label>
+          <p style={{ paddingTop: "20px" }}>Year: {selectedYear}</p>
+          <p>Name: {capitalizeName(orders[0].name)}</p>
+        </div>
+        <div className="order__chart">
+          <div>
+            <div></div>
+          </div>
+          <div>Date</div>
+          <div>Check #</div>
+          <div>Cartridge</div>
+          <div>Offering</div>
+          <div>Thanksgiving</div>
+          <div>Self & World</div>
+          <div>Building</div>
+
+          <div>Total</div>
+          <div></div>
+          {showReport && renderOrderRow(orders)}
+
+          <div></div>
+          <div style={{ borderRight: "none" }}></div>
+          <div style={{ justifyContent: "end" }}>Subtotal Check:</div>
+          <div>$ {showReport ? totals.cartridge.check.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.offering.check.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.thanksGiving.check.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.selfDenial.check.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.buildingFund.check.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.subTotalCheck.toFixed(2) : 0}</div>
+          <div></div>
+
+          <div></div>
+          <div style={{ borderRight: "none" }}></div>
+          <div style={{ justifyContent: "end" }}>Subtotal Cash:</div>
+          <div>$ {showReport ? totals.cartridge.cash.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.offering.cash.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.thanksGiving.cash.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.selfDenial.cash.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.buildingFund.cash.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.subTotalCash.toFixed(2) : 0}</div>
+          <div></div>
+
+          <div></div>
+          <div style={{ borderRight: "none" }}></div>
+          <div style={{ justifyContent: "end" }}>Total:</div>
+          <div>$ {showReport ? totals.cartridge.total.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.offering.total.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.thanksGiving.total.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.selfDenial.total.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.buildingFund.total.toFixed(2) : 0}</div>
+          <div>$ {showReport ? totals.total.toFixed(2) : 0}</div>
+          <div></div>
+        </div>
+      </div>
+    );
+  };
+  const renderLetter = (userIds, orders) => {
+    return (
+      <div>
+        <span>LOGO</span>
+        <p>Dear {"name"}</p>
+      </div>
+    );
+  };
+
   const renderTableByUser = (userIds, orders) => {
     if (!userIds || !orders) return;
     return userIds.map((id) => {
@@ -151,79 +226,13 @@ const GeneratedReport = ({ orders, users, selectedUser, selectedYear }) => {
               <div></div>
             </div>
           </div>
-          <div className="page-break">break</div>
-          <p>aaang</p>
+          <div className="page-break"></div>
+          {renderLetter()}
+          <div className="page-break"></div>
         </React.Fragment>
       );
     });
   };
-
-  // const renderTable = () => {
-  //   return (
-  //     <>
-  //       <div
-  //         className="flex__vertical"
-  //         style={{
-  //           minHeight: "500px",
-  //           backgroundColor: "white",
-  //         }}
-  //       >
-  //         <label>Generated Report</label>
-  //         <div className="order__chart">
-  //           <div>
-  //             <div></div>
-  //           </div>
-  //           <div>Date</div>
-  //           <div>Check #</div>
-  //           <div>Cartridge</div>
-  //           <div>Offering</div>
-  //           <div>Thanksgiving</div>
-  //           <div>Self & World</div>
-  //           <div>Building</div>
-
-  //           <div>Total</div>
-  //           <div></div>
-  //           {showReport && renderOrderRow(filteredOrders)}
-  //           {renderEmptyRow()}
-
-  //           <div></div>
-  //           <div style={{ borderRight: "none" }}></div>
-  //           <div style={{ justifyContent: "end" }}>Subtotal Check:</div>
-  //           <div>$ {showReport ? totals.cartridge.check.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.offering.check.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.thanksGiving.check.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.selfDenial.check.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.buildingFund.check.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.subTotalCheck.toFixed(2) : 0}</div>
-  //           <div></div>
-
-  //           <div></div>
-  //           <div style={{ borderRight: "none" }}></div>
-  //           <div style={{ justifyContent: "end" }}>Subtotal Cash:</div>
-  //           <div>$ {showReport ? totals.cartridge.cash.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.offering.cash.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.thanksGiving.cash.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.selfDenial.cash.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.buildingFund.cash.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.subTotalCash.toFixed(2) : 0}</div>
-  //           <div></div>
-
-  //           <div></div>
-  //           <div style={{ borderRight: "none" }}></div>
-  //           <div style={{ justifyContent: "end" }}>Total:</div>
-  //           <div>$ {showReport ? totals.cartridge.total.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.offering.total.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.thanksGiving.total.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.selfDenial.total.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.buildingFund.total.toFixed(2) : 0}</div>
-  //           <div>$ {showReport ? totals.total.toFixed(2) : 0}</div>
-  //           <div></div>
-  //         </div>
-  //       </div>
-  //       <div className="page-break">break</div>
-  //     </>
-  //   );
-  // };
 
   const render = () => {
     // return <>{renderTable()}</>;
